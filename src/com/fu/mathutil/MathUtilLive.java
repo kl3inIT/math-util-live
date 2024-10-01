2f4a9617b787157db1bb37da3d0ddb5f7fbc0de5
kl3inIT <dathip04@gmail.com>
2024-10-01 21:29:03 +0700

    up code to sever1

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -5,6 +5,8 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("expected: 5! = 120; actual: " + result);
+        
+        System.out.println("");
     }
 
 }
